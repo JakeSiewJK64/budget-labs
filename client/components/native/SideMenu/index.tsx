8e75613449ed01d4965f9b5f809c633df1cd0bd7
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-13 08:19:40 +0800

    add side menu

---
+++ client/components/native/SideMenu/index.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Card } from "@/components/ui/card";
+
+const SideMenu = ({
+  routes,
+}: {
+  routes: { path: string; label: string }[];
+}) => {
+  return (
+    <Card className="flex justify-center">
+      <NavigationMenu orientation="vertical">
+        <NavigationMenuList className="flex-col items-start space-x-0">
+          {routes.map((route, index) => (
+            <NavigationMenuItem key={`${route.path}-${route.label}`}>
+              <Link href={route.path} legacyBehavior passHref>
+                <NavigationMenuLink
+                  className={navigationMenuTriggerStyle({
+                    className: `w-[12rem] truncate border-b-2 rounded-none`,
+                  })}
+                >
+                  {route.label}
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </Card>
+  );
+};
+
+export default SideMenu;
