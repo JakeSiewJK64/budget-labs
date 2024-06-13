d8bde511a98f35959e2cb0a75d2e628b98e18328
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-13 20:37:18 +0800

    update side menu

--- client/components/native/SideMenu/index.tsx
+++ client/components/native/SideMenu/index.tsx
@@ -19,12 +19,12 @@ const SideMenu = ({
     <Card className="flex justify-center">
       <NavigationMenu orientation="vertical">
         <NavigationMenuList className="flex-col items-start space-x-0">
-          {routes.map((route, index) => (
+          {routes.map((route) => (
             <NavigationMenuItem key={`${route.path}-${route.label}`}>
               <Link href={route.path} legacyBehavior passHref>
                 <NavigationMenuLink
                   className={navigationMenuTriggerStyle({
-                    className: `w-[12rem] truncate border-b-2 rounded-none`,
+                    className: "w-[10rem] truncate border-b-2 rounded-none",
                   })}
                 >
                   {route.label}
