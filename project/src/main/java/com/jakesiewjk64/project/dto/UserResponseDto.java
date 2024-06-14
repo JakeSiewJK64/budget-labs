49e74a57078660a780e9681fef11bf249c84a299
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-14 19:01:51 +0800

    user dto - add user id

--- project/src/main/java/com/jakesiewjk64/project/dto/UserResponseDto.java
+++ project/src/main/java/com/jakesiewjk64/project/dto/UserResponseDto.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserResponseDto {
+  private int id;
   private String email;
   private String firstname;
   private String lastname;
