873ff74b11981ec11f293b62335c95b751430982
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-14 18:56:00 +0800

    add get current user endpoint

--- project/src/main/java/com/jakesiewjk64/project/dto/UserResponseDto.java
+++ project/src/main/java/com/jakesiewjk64/project/dto/UserResponseDto.java
@@ -2,9 +2,11 @@ package com.jakesiewjk64.project.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserResponseDto {
   private String email;
   private String firstname;
