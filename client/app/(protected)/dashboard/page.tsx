14b7a4b2d7e9692e57118076435b24604dc5d0a8
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-14 08:52:14 +0800

    update dashboard paeg

--- client/app/(protected)/dashboard/page.tsx
+++ client/app/(protected)/dashboard/page.tsx
@@ -1,7 +1,123 @@
-import React from "react";
+import { DashboardCards } from "@/components/native";
+import { Button } from "@/components/ui/button";
+import {
+  TableCaption,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+  TableFooter,
+  Table,
+} from "@/components/ui/table";
+import { MdCalendarMonth } from "react-icons/md";
+import { CiCoinInsert } from "react-icons/ci";
+import { FaMoneyBill } from "react-icons/fa";
+
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+];
 
 const Page = () => {
-  return <div>dashboard</div>;
+  return (
+    <div className="flex flex-col">
+      <DashboardCards
+        infoArray={[
+          {
+            color: "#f97316",
+            title: "Total spending this month",
+            value: 1000,
+            icon: <FaMoneyBill color="white" />,
+          },
+          {
+            color: "#ef4444",
+            title: "Highest spending this month",
+            value: 20,
+            icon: <MdCalendarMonth color="white" />,
+          },
+          {
+            color: "#4f46e5",
+            title: "Total spending today",
+            value: 10,
+            icon: <CiCoinInsert color="white" />,
+          },
+        ]}
+      />
+      <div className="ml-auto">
+        <Button>New Expense</Button>
+      </div>
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Invoice</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Method</TableHead>
+            <TableHead className="text-right">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {invoices.map((invoice) => (
+            <TableRow key={invoice.invoice}>
+              <TableCell className="font-medium">{invoice.invoice}</TableCell>
+              <TableCell>{invoice.paymentStatus}</TableCell>
+              <TableCell>{invoice.paymentMethod}</TableCell>
+              <TableCell className="text-right">
+                {invoice.totalAmount}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={3}>Total</TableCell>
+            <TableCell className="text-right">$2,500.00</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </div>
+  );
 };
 
 export default Page;
