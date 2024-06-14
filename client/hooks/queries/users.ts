f29f013e3ee5cf63a7704b70ede641c58c84fc42
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-14 20:46:02 +0800

    update users query hooks

--- client/hooks/queries/users.ts
+++ client/hooks/queries/users.ts
@@ -1,15 +1,28 @@
+import { z } from "zod";
+import { UserSchema } from "@/types/user";
+import { PaginatedResponseSchema } from "../../types/global";
 import getAxiosInstance from "@/utils/axiosInstance";
 
-export async function useGetAllUsers() {
+const UserResponseSchema = PaginatedResponseSchema(UserSchema);
+
+export async function useGetAllUsers(): Promise<
+  z.output<typeof UserResponseSchema>
+> {
   const axios = getAxiosInstance();
   const res = await axios.get("/users").catch((err) => {
-    return {
-      data: null,
-      statusText: "Error",
-    };
+    throw new Error(err.response.data.message);
   });
-  const data = res.data;
-  const status = res.statusText;
 
-  return { data, status };
+  return UserResponseSchema.parseAsync(res.data);
+}
+
+export async function useGetCurrentUser(): Promise<
+  z.output<typeof UserSchema>
+> {
+  const axios = getAxiosInstance();
+  const res = await axios.get("/users/getCurrentUser").catch((err) => {
+    throw new Error(err.response.data.message);
+  });
+
+  return UserSchema.parseAsync(res.data);
 }
