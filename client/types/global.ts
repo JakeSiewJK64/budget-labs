06e513eb82b107f81aacb0954f745443a9891ec3
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-15 11:03:24 +0800

    update pagination types

--- client/types/global.ts
+++ client/types/global.ts
@@ -1,5 +1,22 @@
 import { z } from "zod";
 
+export type PaginationURLParam = {
+  page_size?: number;
+  page?: number;
+};
+
+export const PaginationRequestSchema = z.object({
+  page: z.string().transform((value) => {
+    if (value === undefined || value === null) return 0;
+    if (Number.isNaN(value)) return 0;
+    return Number.parseInt(value);
+  }),
+  page_size: z.string().transform((value) => {
+    if (value === undefined || value === null) return 10;
+    return Number.parseInt(value);
+  }),
+});
+
 export const PaginatedResponseSchema = <T>(contentSchema: z.ZodType<T>) =>
   z.object({
     content: z.array(contentSchema),
