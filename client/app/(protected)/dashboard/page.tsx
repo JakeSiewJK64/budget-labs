2e266eeaa723af89fdcc39a885e9bf4276f25e4e
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-15 10:12:05 +0800

    update dashboard

--- client/app/(protected)/dashboard/page.tsx
+++ client/app/(protected)/dashboard/page.tsx
@@ -13,55 +13,25 @@ import {
 import { MdCalendarMonth } from "react-icons/md";
 import { CiCoinInsert } from "react-icons/ci";
 import { FaMoneyBill } from "react-icons/fa";
+import { useGetCurrentUser } from "@/hooks/queries/users";
+import { useGetExpenseById } from "@/hooks/queries/expenses";
 
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
-  },
-];
+const Page = async () => {
+  const user = await useGetCurrentUser();
+  const expenses = await useGetExpenseById({
+    page: 0,
+    page_size: 5,
+    user_id: user.id,
+  });
 
-const Page = () => {
   return (
     <div className="flex flex-col">
+      <p className="my-2">
+        Welcome back,{" "}
+        <strong>
+          {user.first_name} {user.last_name}
+        </strong>
+      </p>
       <DashboardCards
         infoArray={[
           {
@@ -91,21 +61,15 @@ const Page = () => {
         <TableCaption>A list of your recent invoices.</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead>Amount</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          {invoices.map((invoice) => (
-            <TableRow key={invoice.invoice}>
-              <TableCell className="font-medium">{invoice.invoice}</TableCell>
-              <TableCell>{invoice.paymentStatus}</TableCell>
-              <TableCell>{invoice.paymentMethod}</TableCell>
-              <TableCell className="text-right">
-                {invoice.totalAmount}
-              </TableCell>
+          {expenses.content.map((expense) => (
+            <TableRow key={expense.id}>
+              <TableCell>{expense.date}</TableCell>
+              <TableCell>{expense.amount}</TableCell>
             </TableRow>
           ))}
         </TableBody>
