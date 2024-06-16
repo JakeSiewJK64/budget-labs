7f8eb338b6edcb5af820a691ac6662a3a219782d
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-16 13:47:18 +0800

    remove zod erro util

--- client/utils/index.ts
+++ client/utils/index.ts
@@ -1,5 +1,9 @@
-import { formatZodError } from "./formatZodError";
 import { getCookie, removeCookie, setCookie } from "./cookiesUtils";
 import getAxiosInstance from "./axiosInstance";
 
-export { formatZodError, getAxiosInstance, getCookie, removeCookie, setCookie };
+export {
+  getAxiosInstance,
+  getCookie,
+  removeCookie,
+  setCookie,
+};
