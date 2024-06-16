d50a6a134e7b701dd11d11454b5a58469e76163f
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-16 13:29:29 +0800

    add index for utils folder

---
+++ client/utils/index.ts
@@ -0,0 +1,5 @@
+import { formatZodError } from "./formatZodError";
+import { getCookie, removeCookie, setCookie } from "./cookiesUtils";
+import getAxiosInstance from "./axiosInstance";
+
+export { formatZodError, getAxiosInstance, getCookie, removeCookie, setCookie };
