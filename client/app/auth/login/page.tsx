f271870299b7644e98a238a4e77ff675587e2450
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-17 15:36:14 +0800

    update login page styling

--- client/app/auth/login/page.tsx
+++ client/app/auth/login/page.tsx
@@ -1,10 +1,13 @@
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { LoginForm } from "@/components/form";
+import { cn } from "@/lib/utils";
+
+const paddingTop = "min-[1200px]:pt-[10%] max-[1200px]:pt-[45%]";
 
 const Page = () => {
   return (
-    <div className="flex grow">
-      <Card className="w-[50%] mx-auto mt-[15%] mb-[20%] max-w-[30rem] p-[1rem]">
+    <div className={cn(`flex justify-center items-baseline ${paddingTop}`)}>
+      <Card className="w-[50%] max-w-[30rem] p-[1rem]">
         <CardTitle className="text-center py-[10px]">Login</CardTitle>
         <CardContent>
           <LoginForm />
