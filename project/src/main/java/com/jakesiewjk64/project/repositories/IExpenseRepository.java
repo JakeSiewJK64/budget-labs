17e85dbebd91b77b05f138eb8617a94f14210a85
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-17 19:59:05 +0800

    add expense stats endpoint

--- project/src/main/java/com/jakesiewjk64/project/repositories/IExpenseRepository.java
+++ project/src/main/java/com/jakesiewjk64/project/repositories/IExpenseRepository.java
@@ -1,9 +1,15 @@
 package com.jakesiewjk64.project.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.jakesiewjk64.project.models.Expense;
 
 public interface IExpenseRepository extends JpaRepository<Expense, Integer>, JpaSpecificationExecutor<Expense> {
+  @Query(value = "SELECT expense FROM Expense expense WHERE expense.user_id = :user_id AND MONTH(expense.date) = :month AND YEAR(expense.date) = :year")
+  List<Expense> getByYearAndMonth(@Param("year") int year, @Param("month") int month, @Param("user_id") int user_id);
 }
