181ba7a3449f6109f99d18742cc36ccbc51e3818
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-17 15:37:56 +0800

    update register page styling

--- client/app/auth/register/page.tsx
+++ client/app/auth/register/page.tsx
@@ -1,10 +1,13 @@
 import { RegisterForm } from "@/components/form";
 import { Card, CardTitle, CardContent } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+
+const paddingTop = "min-[1200px]:pt-[3%] max-[1200px]:pt-[30%]";
 
 const Page = () => {
   return (
-    <div className="flex grow">
-      <Card className="w-[50%] mx-auto mt-[15%] max-w-[30rem] p-[1rem]">
+    <div className={cn(`flex justify-center items-baseline ${paddingTop}`)}>
+      <Card className="w-[50%] max-w-[30rem] p-[1rem]">
         <CardTitle className="text-center py-[10px]">Register</CardTitle>
         <CardContent>
           <RegisterForm />
