dcacb1b80219b18065f1fcfa44c3c05832f5b533
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-17 11:14:28 +0800

    auth layout - remove layout layer check

--- client/app/auth/layout.tsx
+++ client/app/auth/layout.tsx
@@ -1,23 +1,12 @@
 "use server";
 
-import { getIsTokenExpired } from "@/actions/auth";
 import { Toaster } from "@/components/ui/toaster";
-import { getCookie } from "@/utils/cookiesUtils";
-import { redirect } from "next/navigation";
 
 const Layout = async ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  const authToken = getCookie("token");
-  const tokenExpired = await getIsTokenExpired(authToken?.value);
-  const authenticated = authToken && !tokenExpired;
-
-  if (authenticated) {
-    redirect("/dashboard");
-  }
-
   return (
     <>
       <Toaster />
