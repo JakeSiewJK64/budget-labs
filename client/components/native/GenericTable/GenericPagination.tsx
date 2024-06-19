fe9f10a96bac56e1acb06bfc47d38187193c7838
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-19 09:30:53 +0800

    pagination - use  usesearchparams

--- client/components/native/GenericTable/GenericPagination.tsx
+++ client/components/native/GenericTable/GenericPagination.tsx
@@ -1,3 +1,5 @@
+import { useSearchParams } from "next/navigation";
+import { useCallback } from "react";
 import {
   Pagination,
   PaginationContent,
@@ -8,78 +10,6 @@ import {
   PaginationEllipsis,
 } from "@/components/ui/pagination";
 import { cn } from "@/lib/utils";
-
-const getNumberedPagination = ({
-  page,
-  page_size,
-  totalPages,
-}: {
-  totalPages: number;
-  page_size: number;
-  page: number;
-}) => {
-  const firstPage = (
-    <PaginationItem key={0}>
-      <PaginationLink
-        className={cn(page === 0 && "pointer-events-none")}
-        href={`?page=0&page_size=${page_size}`}
-        isActive={page === 0}
-      >
-        1
-      </PaginationLink>
-    </PaginationItem>
-  );
-  const lastPage = (
-    <PaginationItem key={totalPages}>
-      <PaginationLink
-        className={cn(page === totalPages - 1 && "pointer-events-none")}
-        href={`?page=${totalPages - 1}&page_size=${page_size}`}
-        isActive={page === totalPages - 1}
-      >
-        {totalPages}
-      </PaginationLink>
-    </PaginationItem>
-  );
-
-  if (totalPages === 0) {
-    return [firstPage];
-  }
-
-  const pages = [];
-  const currentPage = page;
-
-  pages.push(firstPage);
-
-  for (let i = 1; i < totalPages - 1; i++) {
-    if (i === currentPage || i === currentPage - 1 || i === currentPage + 1) {
-      pages.push(
-        <PaginationItem key={i}>
-          <PaginationLink
-            href={`?page=${i}&page_size=${page_size}`}
-            isActive={i === page}
-            className={cn(i === page && "pointer-events-none")}
-          >
-            {i + 1}
-          </PaginationLink>
-        </PaginationItem>
-      );
-    }
-
-    if (i === currentPage - 2 || i === currentPage + 2) {
-      pages.push(
-        <PaginationItem key={i}>
-          <PaginationEllipsis />
-        </PaginationItem>
-      );
-    }
-  }
-
-  if (totalPages > 1) {
-    pages.push(lastPage);
-  }
-
-  return pages;
-};
 
 const GenericPagination = ({
   paginationInfo,
@@ -92,28 +22,99 @@ const GenericPagination = ({
   last?: boolean;
   paginationInfo: { page: number; page_size: number };
 }) => {
+  const { page } = paginationInfo;
+  const pathname = useSearchParams();
+
+  const updateURL = ({ key, value }: { key: string; value: string }) => {
+    const newURL = new URLSearchParams(pathname.toString());
+    newURL.set(key, value);
+    return `?${newURL.toString()}`;
+  };
+
+  const getNumberedPagination = useCallback(() => {
+    const firstPage = (
+      <PaginationItem key={0}>
+        <PaginationLink
+          className={cn(page === 0 && "pointer-events-none")}
+          href={updateURL({ key: "page", value: "0" })}
+          isActive={page === 0}
+        >
+          1
+        </PaginationLink>
+      </PaginationItem>
+    );
+    const lastPage = (
+      <PaginationItem key={totalPages}>
+        <PaginationLink
+          className={cn(page === totalPages - 1 && "pointer-events-none")}
+          href={updateURL({ key: "page", value: String(totalPages - 1) })}
+          isActive={page === totalPages - 1}
+        >
+          {totalPages}
+        </PaginationLink>
+      </PaginationItem>
+    );
+
+    if (totalPages === 0) {
+      return [firstPage];
+    }
+
+    const pages = [];
+    const currentPage = page;
+
+    pages.push(firstPage);
+
+    for (let i = 1; i < totalPages - 1; i++) {
+      if (i === currentPage || i === currentPage - 1 || i === currentPage + 1) {
+        pages.push(
+          <PaginationItem key={i}>
+            <PaginationLink
+              href={updateURL({ key: "page", value: `${i}` })}
+              isActive={i === page}
+              className={cn(i === page && "pointer-events-none")}
+            >
+              {i + 1}
+            </PaginationLink>
+          </PaginationItem>
+        );
+      }
+
+      if (i === currentPage - 2 || i === currentPage + 2) {
+        pages.push(
+          <PaginationItem key={i}>
+            <PaginationEllipsis />
+          </PaginationItem>
+        );
+      }
+    }
+
+    if (totalPages > 1) {
+      pages.push(lastPage);
+    }
+
+    return pages;
+  }, [totalPages, paginationInfo, last, first]);
+
   return (
     <Pagination>
       <PaginationContent>
         <PaginationItem>
           <PaginationPrevious
             className={first ? "pointer-events-none text-gray-500" : ""}
-            href={`?page_size=${paginationInfo.page_size}&page=${Number(
-              paginationInfo.page - 1
-            )}`}
+            href={updateURL({
+              key: "page",
+              value: String(paginationInfo.page - 1),
+            })}
           />
         </PaginationItem>
-        {getNumberedPagination({
-          page: paginationInfo.page,
-          page_size: paginationInfo.page_size,
-          totalPages,
-        })}
+        {getNumberedPagination()}
         <PaginationItem>
           <PaginationNext
             className={last ? "pointer-events-none text-gray-500" : ""}
-            href={`?page_size=${paginationInfo.page_size}&page=${Number(
-              paginationInfo.page + 1
-            )}`}
+            href={updateURL({
+              key: "page",
+              value: String(paginationInfo.page + 1),
+            })}
           />
         </PaginationItem>
       </PaginationContent>
