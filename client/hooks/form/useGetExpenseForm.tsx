28b1a31a2f0cde061b105fa49cf933dc27ed00a3
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-20 08:18:05 +0800

    expense form - default values

--- client/hooks/form/useGetExpenseForm.tsx
+++ client/hooks/form/useGetExpenseForm.tsx
@@ -2,22 +2,27 @@ import dayjs from "dayjs";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { ExpenseSchema } from "@/types/expense";
 
 const expenseFormSchema = z.object({
   description: z.string().min(1, {
     message: "Description cannot be empty.",
   }),
-  amount: z.string(),
+  amount: z.number(),
   date: z.date(),
 });
 
-const useExpenseForm = () => {
+const useExpenseForm = ({
+  defaultValues,
+}: {
+  defaultValues?: z.infer<typeof ExpenseSchema>;
+}) => {
   const loginForm = useForm<z.infer<typeof expenseFormSchema>>({
     resolver: zodResolver(expenseFormSchema),
     defaultValues: {
-      amount: "0",
-      date: dayjs().toDate(),
-      description: "",
+      amount: defaultValues?.amount ?? 0,
+      date: dayjs(defaultValues?.date ?? dayjs()).toDate(),
+      description: defaultValues?.description ?? "",
     },
   });
 
